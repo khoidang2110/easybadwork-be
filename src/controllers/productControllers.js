eae3e24b562f1cd8e67542b84281c07002ec687a
Kha Le (GMAP-TECH-TECH POOL-FRONTEND DEV) <kha.le@ivnd.com.vn>
2024-04-24 16:41:51 +0700

    update  req.query

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -2,80 +2,75 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
 const getProductByCategory = async (req, res) => {
   try {
     // Assuming prisma.products represents your product model
-    let { category_name } = req.params;
-
+    let { categoryName, page, size } = req.query;
 
     let products = await prisma.product.findMany({
-    
       include: {
-          category: true,
-          image: true,
-          stock:true, // Include related category information
+        category: true,
+        image: true,
+        stock: true, // Include related category information
       },
+    });
 
-  });
-  
     if (products.length === 0) {
       res.send("No products found");
     } else {
-
-
-      const getProducts = products
-      .map(product => ({
-        product_id: product.product_id*1, // Ensure the field name matches the response
+      const getProducts = products.map((product) => ({
+        product_id: product.product_id * 1, // Ensure the field name matches the response
         name: product.name,
-        price_vnd: product.price_vnd*1,
-        price_usd: product.price_usd*1,
+        price_vnd: product.price_vnd * 1,
+        price_usd: product.price_usd * 1,
         decs_vi: product.decs_vi,
         decs_en: product.decs_en,
         category: product.category.category_name,
-        image: product.image.map(item=>item.img_link),
-        stock:product.stock
-        .filter(item => item.stock > 0) // stock = 0 ko render ra size
-        .map(item=>({
-          size : item.size,
-          stock : item.stock,
-        }))
-    }));
-const filterProduct = getProducts.filter(product=>product.category==category_name)
+        image: product.image.map((item) => item.img_link),
+        stock: product.stock
+          .filter((item) => item.stock > 0) // stock = 0 ko render ra size
+          .map((item) => ({
+            size: item.size,
+            stock: item.stock,
+          })),
+      }));
+      const filterProduct = getProducts.filter(
+        (product) => product.category == categoryName
+      );
+      console.log("getProducts", getProducts);
+      //res.send(products);
+      //  res.send(getProducts);
+      if (categoryName) {
+        res.send(filterProduct);
+      } else {
+        res.send(getProducts);
+      }
+    }
+  } catch (error) {
+    console.log("error", error);
+    res.status(500).send(`Internal server error: ${error}`);
+  }
+};
 
-//res.send(products);
-    //  res.send(getProducts);
-    res.send(filterProduct);
+const findProductByName = async (req, res) => {
+  try {
+    let { keyword } = req.query;
+
+    const products = await prisma.product.findMany({
+      where: {
+        name: {
+          contains: keyword,
+          mode: "insensitive", // case-insensitive search ( uppercase)
+        },
+      },
+    });
+    if (products.length > 0) {
+      res.send(products);
+    } else {
+      console.log(`No products found with name ${keyword}`);
     }
   } catch (error) {
     res.status(500).send(`Internal server error: ${error}`);
   }
 };
-
-const findProductByName = async (req,res)=>{
-
-
-
-try {
-  let { keyword } = req.params;
-
-  const products = await prisma.product.findMany({
-    where: {
-      name: {
-        contains: keyword,
-        mode: 'insensitive', // case-insensitive search ( uppercase)
-      },
-    },
-  });
-  if(products.length>0){
-   res.send(products);
-  }else {
-    console.log(`No products found with name ${keyword}`)
-  }
-  
-} catch (error) {
-  res.status(500).send(`Internal server error: ${error}`);
-}
-}
-  export { getProductByCategory,findProductByName };
-  
+export { getProductByCategory, findProductByName };
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,21 +1,16 @@
 import express from "express";
-import { findProductByName, getProductByCategory,  } from "../controllers/productControllers.js";
-
-
+import {
+  findProductByName,
+  getProductByCategory,
+} from "../controllers/productControllers.js";
 
 const productRoutes = express.Router();
 
-
-
-
-
-
-
-
-
 // liệt kê danh sách sp theo loại (category)
-productRoutes.get('/get-products-category/:category_name',getProductByCategory);
+
+productRoutes.get("/get-products-category", getProductByCategory);
+
 // tìm sản phẩm theo tên
-productRoutes.get('/find-product/:keyword',findProductByName);
+productRoutes.get("/find-product", findProductByName);
 
 export default productRoutes;
