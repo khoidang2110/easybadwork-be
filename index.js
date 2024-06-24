aa06ee06af53cf708d9bc7ed47bcf69bc63c3a69
khoidang2110 <khoidang2110@gmail.com>
2024-06-24 20:46:40 +0700

    cors

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const port = 8081;
 app.use(express.json()); //middleware để  parse body string -> body json
 app.use(express.static(".")); // middleware để xác định nơi lưu file ( dùng lấy hình show browser)
 app.use(cors()); //middleware cho tất cả các request từ bên ngoài vào( front end) để trước rootroutes để bypass (backend chạy từ trên xuống)
+
+const corsOptions = {
+    origin: 'https://easybadwork-fe.vercel.app', // Replace with your frontend domain
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  };
+app.use(cors(corsOptions));
+
+
+
+
+
 app.use(rootRoutes);
 
 
